86209997bfa88c4ead68ede081b9bf704489aca7
shreyash <shre240101020@iiitmanipur.ac.in>
2026-06-30 11:42:33 +0530

    Time: 7 ms (41.96%), Space: 13.9 MB (65.4%) - LeetHub

--- 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
-    long long countVowels(string s) {
-        long long res = 0; 
-        int n = s.size();
-        for (int i = 0; i < n; ++i)
-            if (string("aeiou").find(s[i]) != string::npos)
-                res += 1LL * (i + 1) * (n - i);
-        return res;
+    bool isVowel(char c){
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+            return true;
+        }
+
+        return false;
+    }
+
+    long long countVowels(string word) {
+        int n = word.length();
+        long long cnt = 0;
+
+        for(int i=0; i<n; i++){
+            if(isVowel(word[i])){
+                cnt += 1LL * (i+1) * (n-i);
+            }
+        }
+
+        return cnt;
+
     }
 };
