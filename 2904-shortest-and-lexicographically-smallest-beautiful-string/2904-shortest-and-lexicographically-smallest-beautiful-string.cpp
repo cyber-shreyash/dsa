0d8e854a2b063e5b9ee8ab2a263ca9b6015dd62b
shreyash <shreyashkushwaha2006@gmail.com>
2026-08-26 06:56:11 +0530

    Time: 0 ms (100%), Space: 8.6 MB (82.87%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -2,51 +2,31 @@ class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
         int n = s.size();
-        string ki = "";
-        unordered_map<char, int> mp;
-        vector<string> v;
         int l = 0;
-        int maxi = 0;
         int count = 0;
-
+        string ans = "";
         for (int r = 0; r < n; r++) {
-            mp[s[r]]++;
             if (s[r] == '1') {
                 count++;
-            }
-            if (s[r] == '1') {
-                ki += '1';
-            } else {
-                ki += '0';
             }
             while (count > k) {
                 if (s[l] == '1') {
                     count--;
                 }
-                mp[s[l]]--;
-                if (mp[s[l]] == 0) {
-                    mp.erase(s[l]);
-                }
-                ki.erase(0, 1);
                 l++;
             }
-            maxi = max(maxi, r - l + 1);
             while (count == k && s[l] == '0') {
-                ki.erase(0, 1);
                 l++;
             }
             if (count == k) {
-                v.push_back(ki);
+                string curr = s.substr(l, r - l + 1);
+                if (ans == "" ||
+                    curr.length() < ans.length() ||
+                    (curr.length() == ans.length() && curr < ans)) {
+                    ans = curr;
+                }
             }
         }
-        sort(v.begin(), v.end(), [](string a, string b) {
-            if (a.length() != b.length())
-                return a.length() < b.length();
-
-            return a < b;
-        });
-        if (v.empty())
-            return "";
-        return v[0];
+        return ans;
     }
 };
