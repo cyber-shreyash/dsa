7aff188cf81ea77645e4001074be8ee41c2e2ea7
shreyash <shreyashkushwaha2006@gmail.com>
2026-08-30 06:06:44 +0530

    Time: 0 ms (100%), Space: 92.1 MB (58.82%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -3,24 +3,17 @@ public:
     int minimumDeletions(vector<int>& nums) {
         int mini = INT_MAX;
         int maxi = INT_MIN;
+        int minindex=0;
+        int maxindex=0;
         int n = nums.size();
         for (int i = 0; i < n; i++) {
             if (mini > nums[i]) {
                 mini = nums[i];
+                minindex = i;
             }
             if (maxi < nums[i]) {
                 maxi = nums[i];
-            }
-        }
-        int minindex = n;
-        int maxindex = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == maxi) {
                 maxindex = i;
-            }
-            if (nums[i] == mini) {
-                minindex = i;
             }
         }
         int l = min(minindex, maxindex);
