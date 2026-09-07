9a3d0365869e8971aa4a6f57125ef42145f70157
shreyash <shreyashkushwaha2006@gmail.com>
2026-09-07 05:42:18 +0530

    Time: 39 ms (20.25%), Space: 27 MB (52.6%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int solve(int i, int j, string &s, string &t,
-              vector<vector<int>>& memo) {
-
-        if(j == t.size()) return 1;
-        if(i == s.size()) return 0;
-
-        if(memo[i][j] != -1) return memo[i][j];
-
-        int notTake = solve(i + 1, j, s, t, memo);
-        int take = 0;
-        if(s[i] == t[j]){
-            take = solve(i + 1, j + 1, s, t, memo);
+    int help(int i, int j, int n, int m, string& s, string& t,vector<vector<int>>&dp) {
+        if (j == m) {
+            return 1;
         }
-        return memo[i][j] = take + notTake;
+        if (i == n) {
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int take = 0;
+        if (s[i] == t[j]) {
+            take = help(i + 1, j + 1, n, m, s, t,dp);
+        }
+        int nt = help(i + 1, j, n, m, s,t,dp);
+        return dp[i][j]=take + nt;
     }
-
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> memo(n, vector<int>(m, -1));
-        return solve(0, 0, s, t, memo);
+        vector<vector<int>>dp(n+1, vector<int>(m+1,-1));
+        return help(0, 0, n, m, s, t,dp);
     }
 };
