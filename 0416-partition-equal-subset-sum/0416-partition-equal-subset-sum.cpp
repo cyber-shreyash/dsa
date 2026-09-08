8b8aa9a77d92ddc68d8d223ad9333650fc90e51f
shreyash <shreyashkushwaha2006@gmail.com>
2026-09-08 06:10:33 +0530

    Time: 127 ms (60.44%), Space: 17.5 MB (62.22%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -6,21 +6,24 @@ public:
         for (int x : nums) {
             sum += x;
         }
-        int target = sum / 2;
-        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
-
-        if (sum % 2 != 0) {
+        if (sum % 2 == 1) {
             return false;
         }
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = true;
+        int target = sum / 2;
+        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1));
+
+        for (int i = 0; i < n+1; i++) {
+            dp[i][0] =true;
         }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= target; j++) {
+        for (int j = 0; j <target + 1; j++) {
+            dp[0][j] = false;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < target + 1; j++) {
                 if (nums[i - 1] <= j) {
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
-                }
-                else{
+                    dp[i][j] = dp[i - 1][j - nums[i-1]] || dp[i - 1][j];
+                } else {
                     dp[i][j] = dp[i - 1][j];
                 }
             }
