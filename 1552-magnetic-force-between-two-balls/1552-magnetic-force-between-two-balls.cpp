f9319d6a37115be6edf39be2f0ceb95dfc8bcf01
shreyash <shreyashkushwaha2006@gmail.com>
2026-09-19 08:50:04 +0530

    Time: 47 ms (68.91%), Space: 61.7 MB (37.33%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
-    bool ifpossible(vector<int>& position, int m,int &mid){
-        int count=1;
+    bool ifpossible(vector<int>& position, int m, int& mid) {
+        int count =
+            1; // considering that we are placing the ball in the first basket
+               // which has the lowest index (sorted array) no we can find the
+               // maxm distance using the other baskets to place the balls
         int n = position.size();
-        int prev=position[0];
-        for(int i =1;i<n;i++){
-            if(position[i]-prev>=mid){
+        int prev = position[0];
+        for (int i = 1; i < n; i++) {
+            if (position[i] - prev >= mid) {
                 count++;
-                prev=position[i];
+                prev = position[i];
             }
         }
-        if(count>=m){
+        if (count >= m) {
             return true;
         }
         return false;
     }
     int maxDistance(vector<int>& position, int m) {
         int n = position.size();
-        sort(position.begin(),position.end());
-        int ans=0;
+        sort(position.begin(), position.end());
+        int ans = 0;
         int l = 1;
-        int r = position[n-1]-position[0];
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(ifpossible(position,m,mid)){
-                ans=mid;
-                l=mid+1;
-            }
-            else{
-                r=mid-1;
+        int r = position[n - 1] - position[0];
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (ifpossible(position, m, mid)) {
+                ans = mid;
+                l = mid + 1; // to search maximum possible distance so checking
+                             // by increasing distance
+            } else {
+                r = mid - 1;
             }
         }
-    return ans;
+        return ans;
     }
 };
